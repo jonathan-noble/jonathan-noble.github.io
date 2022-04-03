c204e689559f70283f8ea0e99bbd3515fa77d9ee
jonathan-noble <c15487922@mydit.ie>
2022-04-03 01:11:24 +0100

    test commit

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -12,7 +12,7 @@ function AboutMe() {
             
             <br/>
             <p>
-                I was born in the Philippines, but I have been living in Dublin, Ireland more than half of my life now. I have achieved my Bachelor's degree
+                I was born in the Philippines, and I have been living in Dublin, Ireland more than half of my life now. I have achieved my Bachelor's degree
                 in <b>Computer Science International</b> at Technological University Dublin. During my third year Erasmus programme in Darmstadt, Germany,
                 I have also attained another Bachelor's degree in <b>Computer Science</b> at Darmstadt University of Applied Sciences.
             </p>     
